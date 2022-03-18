f82416ccf0a7b9099757f921e9a05f958f7a7995
luogeger <xiaoqing1232@163.com>
2022-03-18 22:07:26 +0800

    init

---
+++ src/main/java/com/first/AppMain.java
@@ -0,0 +1,23 @@
+package com.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController("")
+@SpringBootApplication
+public class AppMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class);
+    }
+
+    @GetMapping("/test")
+    public String test () {
+        LocalDateTime now = LocalDateTime.now();
+        return "Hello Spring Boot! " + now;
+    }
+}
